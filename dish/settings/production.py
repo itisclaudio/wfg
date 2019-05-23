f8aa6ee97a121ebfe640d615c05911cbaec75b13
claudio roman <claudio.roman@eeoc.gov>
2019-05-23 14:38:52 -0400

    Settings AWS_QUERYSTRING_AUTH = False

--- dish/settings/production.py
+++ dish/settings/production.py
@@ -60,6 +60,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_QUERYSTRING_AUTH = False#Doesn't add signature after media files
 
 #To send emails
 EMAIL_USE_TLS = True
