ca1cfe44c81a78fc72578b760ba8d1b4959bd31d
claudio roman <claudio.roman@eeoc.gov>
2019-05-28 16:01:46 -0400

    Fixing AmazonS3 21

--- dish/settings/production.py
+++ dish/settings/production.py
@@ -29,7 +29,7 @@ MIDDLEWARE_CLASSES = (
 	'django_mobile.middleware.SetFlavourMiddleware',
 )
 
-BASE_URL = 'https://wfgs.herokuapp.com/' # By me: for global_constants
+#BASE_URL = 'https://wfgs.herokuapp.com/' # By me: for global_constants
 
 import dj_database_url
 #db_from_env = dj_database_url.config()
