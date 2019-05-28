071507e3beef08f617294cfffe12db09be8fb256
claudio roman <claudio.roman@eeoc.gov>
2019-05-28 15:58:22 -0400

    Fixing AmazonS3 20

--- custom_storages.py
+++ custom_storages.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
-#MediaRootS3BotoStorage  = lambda: S3Boto3Storage(location='media')
-class MediaRootS3BotoStorage(S3Boto3Storage):
-	location = settings.MEDIAFILES_LOCATION
+MediaRootS3BotoStorage  = lambda: S3Boto3Storage(location='media')
+#class MediaRootS3BotoStorage(S3Boto3Storage):
+#	location = settings.MEDIAFILES_LOCATION
