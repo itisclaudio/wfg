e3a72c77a2e4c8d5fc9882b269b196bbb4bb2c06
claudio roman <claudio.roman@eeoc.gov>
2019-05-28 15:11:48 -0400

    Fixing AmazonS3 14

--- custom_storages.py
+++ custom_storages.py
@@ -3,4 +3,4 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 #MediaRootS3BotoStorage  = lambda: S3Boto3Storage(location='media')
 class MediaStorage(S3Boto3Storage):
-	location = settings.MEDIA_URL
+	location = settings.MEDIAFILES_LOCATION
