dcd364fce5cad442d7b01f5d803d180d54f9e13f
claudio roman <claudio.roman@eeoc.gov>
2019-05-30 11:56:42 -0400

    Serving media 2

--- custom_storages.py
+++ custom_storages.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
-MediaRootS3BotoStorage  = lambda: S3Boto3Storage(location='media')
+#MediaRootS3BotoStorage  = lambda: S3Boto3Storage(location='media')
 StaticRootS3BotoStorage  = lambda: S3Boto3Storage(location='static')
-#class MediaRootS3BotoStorage(S3Boto3Storage):
-#	location = settings.MEDIAFILES_LOCATION
+class MediaRootS3BotoStorage(S3Boto3Storage):
+	location = settings.MEDIAFILES_LOCATION
