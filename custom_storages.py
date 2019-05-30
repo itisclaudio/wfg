95fd0911e119bc3043a1c3d2653b413e892fb795
claudio roman <claudio.roman@eeoc.gov>
2019-05-30 12:36:20 -0400

    Serving media 6

--- custom_storages.py
+++ custom_storages.py
@@ -4,4 +4,4 @@ from storages.backends.s3boto3 import S3Boto3Storage
 #MediaRootS3BotoStorage  = lambda: S3Boto3Storage(location='media')
 StaticRootS3BotoStorage  = lambda: S3Boto3Storage(location='static')
 class MediaRootS3BotoStorage(S3Boto3Storage):
-	location = settings.MEDIAFILES_LOCATION
+	location = 'media'
