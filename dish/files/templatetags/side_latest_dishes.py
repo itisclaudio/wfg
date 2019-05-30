0eea9011541005cb5555de8d5d5a5f2eb6d9f43d
claudio roman <claudio.roman@eeoc.gov>
2019-05-30 15:07:47 -0400

    Serving media 13

--- dish/files/templatetags/side_latest_dishes.py
+++ dish/files/templatetags/side_latest_dishes.py
@@ -1,5 +1,6 @@
 from dish.files.models import Dish
 from django import template
+from dish import settings #To call MEDIA_URL
 
 register = template.Library()
 
@@ -11,5 +12,5 @@ def show_dishes():
 	#dishes_list_id = Dish.objects.filter(active=True).values_list('id', flat=True).order_by('-datetime')[:4]
 	#photos = Picture.objects.filter(active=1, dish__pk__in = dishes_list_id).select_related('dish','owner__user').prefetch_related('dish__cuisines').order_by('-datetime')[:4]
 	dish = Dish.objects.filter(active=True).prefetch_related('cuisines').select_related('userprofile__user').order_by('-datetime')[:4]
-	return {'latest_dishes': dish}
+	return {'latest_dishes': dish, 'MEDIA_URL':settings.MEDIA_URL}
 	
