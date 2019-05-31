67947b67cd52672072cec147b70b4e350310bbc3
claudio roman <claudio.roman@eeoc.gov>
2019-05-31 14:43:04 -0400

    Fixing serving media 8

--- custom_storages.py
+++ custom_storages.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 StaticRootS3BotoStorage = lambda: S3Boto3Storage(location='static') #'static' is the name of your folder in your bucket
-MediaRootS3BotoStorage = lambda: S3Boto3Storage(location='media')
+#MediaRootS3BotoStorage = lambda: S3Boto3Storage(location='media')
 
-#class MediaRootS3BotoStorage(S3Boto3Storage):
-#	location = 'media'
+class MediaRootS3BotoStorage(S3Boto3Storage):
+	location = 'media'
