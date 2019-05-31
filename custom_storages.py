371c8774a2fb027700dab12d398e04e008f59dec
claudio roman <claudio.roman@eeoc.gov>
2019-05-31 15:00:37 -0400

    Fixing serving media 11

--- custom_storages.py
+++ custom_storages.py
@@ -3,8 +3,8 @@ from dish import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 StaticRootS3BotoStorage = lambda: S3Boto3Storage(location='static') #'static' is the name of your folder in your bucket
-#MediaRootS3BotoStorage = lambda: S3Boto3Storage(location='media')
+MediaRootS3BotoStorage = lambda: S3Boto3Storage(location='media')
 
-class MediaRootS3BotoStorage(S3Boto3Storage):
-	location = settings.MEDIAFILES_LOCATION
+#class MediaRootS3BotoStorage(S3Boto3Storage):
+#	location = settings.MEDIAFILES_LOCATION
 	#location = 'media'
