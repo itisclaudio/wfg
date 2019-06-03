5361e88fdd03e15d357e220a28123346163b4e34
claudio roman <claudio.roman@eeoc.gov>
2019-06-03 17:20:58 -0400

    Serving media 8

--- dish/urls.py
+++ dish/urls.py
@@ -39,7 +39,7 @@ if settings.LOCAL_DEV:
 else:
 	import os
 	urlpatterns = [
-	url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':os.environ.get('MEDIA_URL_AWS')}),
+	url(r'^media/(?P<path>.*)$','django.views.static.serve',{document_root:os.environ.get('MEDIA_URL_AWS')}),
 ] + urlpatterns
 
 #To handle Errors
