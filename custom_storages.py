9c0baff2ea90de2346bae4b08a5a0edea8e85696
claudio roman <claudio.roman@eeoc.gov>
2019-06-03 16:54:54 -0400

    Serving media 4

--- custom_storages.py
+++ custom_storages.py
@@ -4,10 +4,9 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 StaticRootS3BotoStorage = lambda: S3Boto3Storage(location='static') #'static' is the name of your folder in your bucket
-#MediaRootS3BotoStorage = lambda: S3Boto3Storage(location='media')
+MediaRootS3BotoStorage = lambda: S3Boto3Storage(location='media')
 
-@deconstructible
-class MediaRootS3BotoStorage(S3Boto3Storage):
-	bucket_name = settings.AWS_STORAGE_BUCKET_NAME
-	location = settings.MEDIAFILES_LOCATION
+#class MediaRootS3BotoStorage(S3Boto3Storage):
+	#bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+	#location = settings.MEDIAFILES_LOCATION
 	#location = 'media'
