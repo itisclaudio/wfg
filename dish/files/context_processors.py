acc3d707db8161f303c024c6068c3d835083f3b4
claudio roman <claudio.roman@eeoc.gov>
2019-06-06 09:45:42 -0400

    Media 1

--- dish/files/context_processors.py
+++ dish/files/context_processors.py
@@ -6,6 +6,7 @@ from django.conf import settings
 def global_constants (request):
 	# return any necessary values
 	return {
-		'BASE_URL': settings.BASE_URL, #By me: For multiple uses like modals
+		#'BASE_URL': settings.BASE_URL, #By me: For multiple uses like modals
+		'BASE_URL': "http://localhost:8000/"
 		#'TEST_1': "http://localhost:8000/"
 	}
