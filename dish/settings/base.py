48449ca0c8037bdb47877b23d764cea7627ea9d5
claudio roman <claudio.roman@eeoc.gov>
2019-06-06 14:47:25 -0400

    new static 4

--- dish/settings/base.py
+++ dish/settings/base.py
@@ -104,7 +104,7 @@ USE_TZ = False#It was true but to avoid naive datetime using time zone I changed
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
